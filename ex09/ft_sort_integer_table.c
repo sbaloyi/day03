7fb310940b5c978b2198ab4d1483280e7132204f
sbaloyi <sbaloyi@student.wethinkcode.co.za>
2020-06-21 11:55:38 +0200

    initial ex06 commit

---
+++ ex09/ft_sort_integer_table.c
@@ -0,0 +1,28 @@
+void ft_sort_integer_table(int *tab, int size);
+
+void swap(int *a, int *b);
+
+void swap(int *a, int *b) {
+
+	int temp = a*;
+	*a = *b;
+	*b = temp;
+}
+
+void ft_sort_integer_table(int *tab, int size) {
+	
+	int counter = 0;
+	int counter1;
+
+	while (counter < size) {
+		counter1 = counter;
+		while (j < size) {
+			if(tab[counter] >= tab[j]) {
+				swap(&tab[i] >= tab[j]);
+			}
+			++counter1;
+		}
+		++counter;
+	}
+}
+
