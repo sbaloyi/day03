e0b77ba06ad67042a7675be93d1b88547db8a87b
sbaloyi <66995239+sbaloyi@users.noreply.github.com>
2020-06-25 09:39:25 +0200

    Updating the nomenclature of variables

--- ex08/ft_atoi.c
+++ ex08/ft_atoi.c
@@ -3,19 +3,19 @@ int ft_atoi(char *str);
 int ft_atoi(char *str) 
 {
 	int result = 0;
-	int sing = 1;
+	int sign = 1;
 	int counter = 0;
 
 	if (str[0] == '-') 
 	{
 		sign = -1;
-		i++;
+		counter++;
 	}
 
-	while (str[i] != '\0')
+	while (str[counter] != '\0')
 	{
-		result = result * 10 + str[i] - '0';
-		i++;
+		result = result * 10 + str[counter] - '0';
+		counter++;
 	}
 	return (sign * result);
 }
